d1b8a56b758dd6df1cb539087a4b3477cba4fdf1
patrick11514 <ja@patrick115.eu>
2022-10-03 12:24:35 +0200

    Fixed code

--- UPR/init.sh
+++ UPR/init.sh
@@ -2,6 +2,6 @@
 cd $1
 wget https://github.com/geordi/upr-course/raw/master/assets/exercises/templates/ex_01.zip
 unzip ex_01.zip
-cp -r $1/ex_01/* $1/
-rm -rf $1/ex_01
-rm -r $1/ex_01.zip
+cp -r ex_01/. ex_01/.* .
+rm -r ex_01
+rm ex_01.zip
