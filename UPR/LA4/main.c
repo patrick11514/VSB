fb21a048335daa8f709d650e4192ee150bca3ca7
patrick11514 <ja@patrick115.eu>
2022-10-09 09:35:10 +0200

    Read values and save to array

--- UPR/LA4/main.c
+++ UPR/LA4/main.c
@@ -3,26 +3,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void nactiSouradnice(int *x, int *y)
-{
-    int a, b;
-    printf("Zadejte souradnice: ");
-    scanf("%d %d", &a, &b);
-
-    printf("Nacteno (%d,%d)\n", a, b);
-
-    *x = a;
-    *y = b;
-}
-
 int main()
 {
     int x = 0, y = 0;
     int pole[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    scanf("%d", pole);
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", pole + i);
+    }
 
-    printf("%d %d %d", pole[0], pole[1], pole[2]);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", pole[i]);
+    }
 
     return 0;
 }
