5f5ec92b2b1a26d0480a2bc7425a76dfa45bfcda
patrick11514 <ja@patrick115.eu>
2022-10-09 09:50:04 +0200

    Array as parameter to function

--- UPR/LA4/main.c
+++ UPR/LA4/main.c
@@ -3,20 +3,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int sectiPole(int *pole)
+{
+    int soucet = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        soucet += pole[i];
+    }
+
+    return soucet;
+}
+
 int main()
 {
     int x = 0, y = 0;
     int pole[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    for (int i = 0; i < 10; i++)
-    {
-        scanf("%d", pole + i);
-    }
-
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d\n", pole[i]);
-    }
+    printf("Soucet pole je: %d\n", sectiPole(pole));
 
     return 0;
 }
