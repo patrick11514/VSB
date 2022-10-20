ce64ce41cb0252cd8aa0251eac969700e70f8b81
patrick11514 <ja@patrick115.eu>
2022-10-20 23:57:50 +0200

    Edited init script

--- UPR/init.sh
+++ UPR/init.sh
@@ -3,6 +3,6 @@ mkdir $1
 cd $1
 wget https://github.com/geordi/upr-course/raw/master/assets/exercises/templates/ex_01.zip
 unzip ex_01.zip
-cp -r ex_01/. ex_01/.* .
+cp -r ex_01/. ex_01/.vscode .
 rm -r ex_01
 rm ex_01.zip
