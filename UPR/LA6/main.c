3ff7a22cca41bff95cf7c5db174d2b8f00fcd1b3
patrick11514 <ja@patrick115.eu>
2022-10-24 09:41:59 +0200

    Long texts

--- UPR/LA6/main.c
+++ UPR/LA6/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    char t1[] = "Ahoj"; // ekvivalent k řádku t2
+    /*char t1[] = "Ahoj"; // ekvivalent k řádku t2
     char t2[] = {'A', 'h', 'o', 'j', '\0'};
     const char *t3 = "Ahoj";
     char *t4 = "Ahoj";
@@ -23,12 +23,20 @@ int main()
     t1[0] = 'a';
     t2[0] = 'a';
     // t3[0] = 'a';
-    t4[0] = 'a';
+    // t4[0] = 'a';
 
     printf("1: %s\n", t1);
     printf("2: %s\n", t2);
     printf("3: %s\n", t3);
-    printf("4: %s\n", t4);
+    printf("4: %s\n", t4);*/
+
+    const char *dlouhyText = "gmsd sdk mfsd mk sdf skd ds fl sdfl\
+    \
+    fsdkg sd fsd fsd ";
+
+    const char *dlouhyText2 = "gmsd sdk mfsd mk sdf skd ds fl sdfl"
+                              ""
+                              "fsdkg sd fsd fsd";
 
     return 0;
 }
