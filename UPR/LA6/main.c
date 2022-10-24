8299d3c165771b738c718a1691bfca2475f51855
patrick11514 <ja@patrick115.eu>
2022-10-24 09:55:04 +0200

    Compare strings with == and strcmp

--- UPR/LA6/main.c
+++ UPR/LA6/main.c
@@ -2,10 +2,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main()
 {
-    /*char t1[] = "Ahoj"; // ekvivalent k řádku t2
+    char t1[] = "Ahoj"; // ekvivalent k řádku t2
     char t2[] = {'A', 'h', 'o', 'j', '\0'};
     const char *t3 = "Ahoj";
     char *t4 = "Ahoj";
@@ -20,23 +21,18 @@ int main()
     printf("3: %c\n", t3[0]);
     printf("4: %c\n", t4[0]);
 
-    t1[0] = 'a';
-    t2[0] = 'a';
+    // t1[0] = 'a';
+    // t2[0] = 'a';
     // t3[0] = 'a';
     // t4[0] = 'a';
 
-    printf("1: %s\n", t1);
-    printf("2: %s\n", t2);
-    printf("3: %s\n", t3);
-    printf("4: %s\n", t4);*/
+    // printf("1: %s\n", t1);
+    // printf("2: %s\n", t2);
+    // printf("3: %s\n", t3);
+    // printf("4: %s\n", t4);
 
-    const char *dlouhyText = "gmsd sdk mfsd mk sdf skd ds fl sdfl\
-    \
-    fsdkg sd fsd fsd ";
-
-    const char *dlouhyText2 = "gmsd sdk mfsd mk sdf skd ds fl sdfl"
-                              ""
-                              "fsdkg sd fsd fsd";
+    printf("Shoda: %d %d %d %d %d\n", t1 == t1, t1 == t2, t1 == t3, t1 == t4, t1 == "Cau");
+    printf("Shoda strcmp: %d %d %d %d\n", strcmp(t1, t1), strcmp(t1, t2), strcmp(t1, t3), strcmp(t1, t4));
 
     return 0;
 }
