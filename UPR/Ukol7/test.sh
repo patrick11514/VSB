055fd0518290486c4ce3c0fc8309ed217822d679
patrick11514 <ja@patrick115.eu>
2022-11-12 18:23:50 +0100

    Added output to html file

--- UPR/Ukol7/test.sh
+++ UPR/Ukol7/test.sh
@@ -8,9 +8,9 @@ else
         #check if file args exists in that folder   
         if [ -f "$1/args" ]; then
             #put args from file to args for program ./main
-            ./main $(cat "$1/args") < ./$1/stdin
+            ./main $(cat "$1/args") < ./$1/stdin > index.html
         else
-            ./main < ./$1/stdin
+            ./main < ./$1/stdin > index.html
         fi
     else
         echo "Folder does not exist"
