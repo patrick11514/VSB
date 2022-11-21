b63b5c3380765c8409139838f8a39acef8c26791
patrick11514 <ja@patrick115.eu>
2022-11-21 12:27:10 +0100

    Edited test file script for this task

--- UPR/Ukol8/test.sh
+++ UPR/Ukol8/test.sh
@@ -5,7 +5,13 @@ if [ -z "$1" ]; then
 else
     #check if folder exists
     if [ -d "$1" ]; then
-        ./main < ./$1/stdin
+        #check if file args exists in that folder   
+        if [ -f "$1/args" ]; then
+            #put args from file to args for program ./main
+            ./main $(cat "$1/args") < ./$1/stdin
+        else
+            ./main < ./$1/stdin
+        fi
     else
         echo "Folder does not exist"
     fi
