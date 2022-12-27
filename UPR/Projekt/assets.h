21062d90ed9bd7cb95661f7af5345320dc54a423
patrick11514 <ja@patrick115.eu>
2022-12-27 18:52:29 +0100

    Added comments + added coords for each text

--- UPR/Projekt/assets.h
+++ UPR/Projekt/assets.h
@@ -21,10 +21,33 @@ typedef struct Texture
 
 typedef struct MainVariables
 {
+    // main menu paddle position
     Position paddlePosition;
+    // main menu paddle start position
     int paddleStartPosition;
+    // main menu paddle go reverse if true
     bool paddleReverse;
+    // current fps
     int FPS;
+
+    // main menu play button corners LeftTop + RightBottom
+    Position mainMenuPlayLT;
+    Position mainMenuPlayRB;
+    // main menu play button if hover
+    bool mainMenuPlayHover;
+
+    // main menu settings button corners LeftTop + RightBottom
+    Position mainMenuSettingsLT;
+    Position mainMenuSettingsRB;
+    // main menu settings button if hover
+    bool mainMenuSettingsHover;
+
+    // main menu exit button corners LeftTop + RightBottom
+    Position mainMenuExitLT;
+    Position mainMenuExitRB;
+    // main menu exit button if hover
+    bool mainMenuExitHover;
+
 } MainVariables;
 
 bool renderText(SDL_Renderer *renderer, char *text, TTF_Font *font, SDL_Color color, int with, int height, int x, int y);
