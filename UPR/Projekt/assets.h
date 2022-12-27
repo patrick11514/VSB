95f61bad5e9a611b751c38d7309fda0e3e13c10a
patrick11514 <ja@patrick115.eu>
2022-12-27 21:58:32 +0100

    Added window, edited comments + added settings buttons

--- UPR/Projekt/assets.h
+++ UPR/Projekt/assets.h
@@ -21,16 +21,20 @@ typedef struct Texture
 
 typedef struct MainVariables
 {
-    // main menu paddle position
+    SDL_Window *window;
+
+    // paddle under title position
     Position paddlePosition;
-    // main menu paddle start position
+    // paddle under title start position
     int paddleStartPosition;
-    // main menu paddle go reverse if true
+    // paddle under title go reverse if true
     bool paddleReverse;
+
     // current fps
     int FPS;
 
-    // main menu play button corners LeftTop + RightBottom
+    // MAIN MENU BUTTONS
+    //  main menu play button corners LeftTop + RightBottom
     Position mainMenuPlayLT;
     Position mainMenuPlayRB;
     // main menu play button if hover
@@ -42,16 +46,37 @@ typedef struct MainVariables
     // main menu settings button if hover
     bool mainMenuSettingsHover;
 
+    // main menu highscore button corners LeftTop + RightBottom
+    Position mainMenuHighscoreLT;
+    Position mainMenuHighscoreRB;
+    // main menu highscore button if hover
+    bool mainMenuHighscoreHover;
+
     // main menu exit button corners LeftTop + RightBottom
     Position mainMenuExitLT;
     Position mainMenuExitRB;
     // main menu exit button if hover
     bool mainMenuExitHover;
 
+    // SETTING BUTTONS
+    // scale button corners LeftTop + RightBottom
+    Position settingsScaleLT;
+    Position settingsScaleRB;
+    // scale button if hover
+    bool settingsScaleHover;
+
+    // back button corners LeftTop + RightBottom
+    Position settingsBackLT;
+    Position settingsBackRB;
+    // back button if hover
+    bool settingsBackHover;
+
 } MainVariables;
 
 bool renderText(SDL_Renderer *renderer, char *text, TTF_Font *font, SDL_Color color, int with, int height, int x, int y);
 
 bool renderTexture(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y, int w, int h);
+
+bool renderRect(SDL_Renderer *renderer, int x, int y, int w, int h, SDL_Color color);
 
 #endif
