e5de272f450fafd20cbd2ee75315ef9141b8e9e8
patrick11514 <ja@patrick115.eu>
2023-04-19 19:22:18 +0200

    Added -X argument to ssh

--- APPS/lookup.sh
+++ APPS/lookup.sh
@@ -12,7 +12,8 @@ do
     #if ping ok, then connect
     if [ $? -eq 0 ]; then
         echo "Machine $STARTIP$i$ENDIP is up"
-        ssh min0150@$STARTIP$i$ENDIP
+	# start with X11 server
+        ssh -X min0150@$STARTIP$i$ENDIP
         # ask if you want to connect to another machine
         echo "Do you want to connect to another machine? (y/n)"
         read answer
