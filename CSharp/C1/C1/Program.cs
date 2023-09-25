a54a121de61b4e03186df7fb428b6abee567f902
patrick11514 <ja@patrick115.eu>
2023-09-25 10:54:15 +0200

    Imported from VS

---
+++ CSharp/C1/C1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            name = name.ToUpper();
+
+            Console.WriteLine("Ahoj: " + name);
+        }
+    }
+}
