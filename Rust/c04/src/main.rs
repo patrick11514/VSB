6a72f6c450b3bf42de7e53b477d9bbe290c0183a
patrick11514 <ja@patrick115.eu>
2023-11-07 06:51:08 +0100

    Some rust things

--- Rust/c04/src/main.rs
+++ Rust/c04/src/main.rs
@@ -1,5 +1,3 @@
-use std::fmt::Write;
-
 #[derive(Copy, Clone)]
 enum MemoryCell {
     Undefined,
@@ -17,58 +15,91 @@ enum WriteExprError {
     OutOfBounds { index: usize },
 }
 
+struct CpuBuilder {
+    register_count: usize,
+    memory_size: usize,
+    param3: usize,
+}
+
+impl CpuBuilder {
+    fn new() -> Self {
+        Self {
+            register_count: 16,
+            memory_size: 1024,
+            param3: 0,
+        }
+    }
+
+    fn register_count(self, register_count: usize) -> Self {
+        Self {
+            register_count,
+            ..self
+        }
+    }
+
+    fn memory_size(self, memory_size: usize) -> Self {
+        Self {
+            memory_size,
+            ..self
+        }
+    }
+
+    fn zeroed(self) -> Cpu {
+        let Self {
+            register_count,
+            memory_size,
+            param3,
+        } = self;
+        Cpu {
+            registers: vec![MemoryCell::Defined(param3); register_count],
+            memory: vec![MemoryCell::Defined(param3); memory_size],
+        }
+    }
+
+    fn undefined(self) -> Cpu {
+        Cpu {
+            registers: vec![MemoryCell::Undefined; self.register_count],
+            memory: vec![MemoryCell::Undefined; self.memory_size],
+        }
+    }
+}
+
 struct Cpu {
-    registers: [MemoryCell; 16],
-    memory: [MemoryCell; 1024],
+    registers: Vec<MemoryCell>,
+    memory: Vec<MemoryCell>,
 }
 
 impl Cpu {
-    fn undefined() -> Self {
-        Cpu {
-            registers: Vec<MemoryCell>,
-            memory: Vec<MemoryCell>,
-        }
-    }
-
-    fn zeroed() -> Self {
-        Cpu {
-            registers: [MemoryCell::Defined(0); 16],
-            memory: [MemoryCell::Defined(0); 1024],
-        }
-    }
-
     fn read(&self, expr: ReadExpr) -> Result<u8, ReadExprError> {
         let (slice, address) = match expr {
             ReadExpr::Constant(value) => return Ok(value),
             ReadExpr::Addr(address) => match address {
-                AddrExpr::Memory(addr) => (self.memory.as_slice(), addr as usize),
                 AddrExpr::Register(addr) => (self.registers.as_slice(), addr as usize),
+                AddrExpr::Memory(addr) => (self.memory.as_slice(), addr as usize),
             },
         };
-
         match slice.get(address) {
             Some(cell) => match cell {
-                MemoryCell::Undefined => Err(ExprError::Undefined),
+                MemoryCell::Undefined => Err(ReadExprError::Undefined),
                 MemoryCell::Defined(value) => Ok(*value),
             },
-            None => Err(ExprError::OutOfBounds { index: address }),
+            None => Err(ReadExprError::OutOfBounds { index: address }),
         }
     }
 
-    fn write(&mut self, expr: WriteExpr, value: u8) -> Result<(), WriteExpr> {
-        let (slice, address) = match expr {
+    fn write(&mut self, expr: WriteExpr, value: u8) -> Result<(), WriteExprError> {
+        let (slice, address): (&mut [MemoryCell], usize) = match expr {
             WriteExpr::Addr(address) => match address {
-                AddrExpr::Memory(addr) => (self.memory.as_mut(), addr as usize),
                 AddrExpr::Register(addr) => (self.registers.as_mut(), addr as usize),
+                AddrExpr::Memory(addr) => (self.memory.as_mut(), addr as usize),
             },
         };
-
         match slice.get_mut(address) {
-            Some(cell) => match cell {
-                MemoryCell::Undefined => Err(ExprError::Undefined),
-                MemoryCell::Defined(value) => Ok(*value),
-            },
-            None => Err(ExprError::OutOfBounds { index: address }),
+            Some(cell) => {
+                *cell = MemoryCell::Defined(value);
+                Ok(())
+            }
+            None => Err(WriteExprError::OutOfBounds { index: address }),
         }
     }
 }
@@ -77,13 +108,35 @@ enum AddrExpr {
     Register(u8),
     Memory(u32),
 }
+
 enum ReadExpr {
     Constant(u8),
     Addr(AddrExpr),
 }
 
+impl ReadExpr {
+    fn constant(value: u8) -> Self {
+        Self::Constant(value)
+    }
+    fn memory(address: u32) -> Self {
+        Self::Addr(AddrExpr::Memory(address))
+    }
+    fn register(index: u8) -> Self {
+        Self::Addr(AddrExpr::Register(index))
+    }
+}
+
 enum WriteExpr {
     Addr(AddrExpr),
+}
+
+impl WriteExpr {
+    fn memory(address: u32) -> Self {
+        Self::Addr(AddrExpr::Memory(address))
+    }
+    fn register(index: u8) -> Self {
+        Self::Addr(AddrExpr::Register(index))
+    }
 }
 
 enum Instruction {
@@ -93,33 +146,52 @@ enum Instruction {
 
 #[derive(Debug)]
 enum ExecuteError {
-    BadExpression(ReadExprError),
+    ReadExpr(ReadExprError),
+    WriteExpr(WriteExprError),
 }
 
-impl From<ExprError> for ExecuteError {
-    fn from(value: ExprError) -> Self {
-        Self::BadExpression(value)
+impl From<ReadExprError> for ExecuteError {
+    fn from(value: ReadExprError) -> Self {
+        Self::ReadExpr(value)
+    }
+}
+
+impl From<WriteExprError> for ExecuteError {
+    fn from(value: WriteExprError) -> Self {
+        Self::WriteExpr(value)
     }
 }
 
 fn execute_instruction(cpu: &mut Cpu, inst: Instruction) -> Result<(), ExecuteError> {
     match inst {
         Instruction::Print(expr) => {
-            /*let value = match cpu.evaluate_expr(expr) {
-                Ok(value) => value,
-                Err(error) => return Err(error.into()),
-            }; stejný kód => */
             let value = cpu.read(expr)?;
-
             println!("{value}");
         }
-    }
-
+        Instruction::Set { dest, src } => {
+            let value = cpu.read(src)?;
+            cpu.write(dest, value)?;
+        }
+    };
     Ok(())
 }
 
+enum ParseError {}
+
+fn parse(input: &str) -> Result<Vec<Instruction>, ParseError> {
+    todo!();
+}
+
 fn main() {
-    let mut cpu = Cpu::undefined();
-    let inst = Instruction::Print(ReadExpr::Constant(5));
+    let mut cpu = CpuBuilder::new()
+        .register_count(16)
+        .memory_size(128)
+        .zeroed();
+
+    let inst = Instruction::Set {
+        dest: WriteExpr::memory(5),
+        src: ReadExpr::constant(50),
+    };
     execute_instruction(&mut cpu, inst).unwrap();
+    execute_instruction(&mut cpu, Instruction::Print(ReadExpr::memory(5))).unwrap();
 }
