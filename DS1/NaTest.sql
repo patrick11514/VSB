7dd1c52ebce0d14fe253b8fbe5554152a0337c60
patrick11514 <ja@patrick115.eu>
2023-11-07 06:51:32 +0100

    Prepare for the test

--- DS1/NaTest.sql
+++ DS1/NaTest.sql
@@ -49,3 +49,84 @@ JOIN organ org ON z.id_of=org.id_organ AND cl_funkce = 0
 WHERE org.nazev_organu_cz = 'Poslanecká sněmovna' AND
       year(org.od_organ) = 2021 AND
       z.do_o is NULL
+
+
+
+-- REAL PRIPRAVA XDD
+
+SELECT pred, jmeno, prijmeni, DATEDIFF(YEAR, narozeni, CURRENT_TIMESTAMP) as vek
+FROM osoba
+WHERE DATEDIFF(YEAR, narozeni, CURRENT_TIMESTAMP) < 30
+
+SELECT o.pohlavi, COUNT(*) pocet, (CAST(COUNT(*) AS FLOAT) / (
+    SELECT COUNT(*) FROM osoba
+    )) * 100 AS pomer
+FROM osoba o
+GROUP BY o.pohlavi
+
+
+SELECT id_organ, COUNT(*) pocet
+FROM poslanec
+GROUP BY id_organ
+ORDER BY id_organ
+
+
+SELECT id_organ
+FROM poslanec p
+JOIN osoba o ON p.id_osoba = o.id_osoba
+WHERE o.jmeno = 'Andrej' AND
+      o.prijmeni = 'Babiš'
+
+SELECT o.jmeno, o.prijmeni, p.id_kandidatka
+FROM poslanec p
+JOIN osoba o ON p.id_osoba = o.id_osoba
+WHERE p.id_organ = 173
+ORDER BY id_kandidatka
+
+SELECT id_kandidatka, COUNT(*) pocet
+FROM poslanec p
+WHERE p.id_organ = 173
+GROUP BY id_kandidatka
+
+SELECT distinct nazev_typ_org_cz
+FROM organ o
+JOIN typ_organu tyo ON o.id_typ_org = tyo.id_typ_org
+WHERE o.rodic_id_organ = 173
+
+SELECT distinct nazev_organu_cz
+FROM organ o
+JOIN typ_organu tyo ON o.id_typ_org = tyo.id_typ_org
+WHERE rodic_id_organ = 173 AND
+      tyo.nazev_typ_org_cz = 'Klub'
+
+
+
+--
+
+
+SELECT distinct s.id_schuze, s.schuze, s.od_schuze
+FROM schuze s
+JOIN hlasovani h ON s.schuze = h.schuze
+LEFT JOIN zmatecne z ON z.id_hlasovani = h.id_hlasovani
+WHERE DATEPART(YEAR, s.do_schuze) = 2020 AND
+      z.id_hlasovani IS NOT NULL
+ORDER BY s.id_schuze
+
+SELECT *
+FROM schuze s
+JOIN hlasovani h ON s.schuze = h.schuze
+LEFT JOIN zmatecne z ON z.id_hlasovani = h.id_hlasovani
+WHERE DATEPART(YEAR, s.do_schuze) = 2020 AND
+      z.id_hlasovani IS NULL AND
+      s.id_schuze = 521
+
+
+SELECT distinct os.prijmeni, os.jmeno, o.id_organ, COUNT(*) pocet
+FROM organ o
+JOIN poslanec p ON o.rodic_id_organ = p.id_organ
+JOIN osoba os ON p.id_osoba = os.id_osoba
+JOIN zmatecne_hlasovani_poslanec zhp ON p.id_osoba = zhp.id_osoba
+GROUP BY zhp.id_hlasovani, o.rodic_id_organ, os.prijmeni, os.jmeno, o.id_organ
+ORDER BY pocet DESC
+
+
