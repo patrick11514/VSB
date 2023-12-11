a6832bbdbd231fb6f6aaa2955a4018fbb5c1d8ac
patrick11514 <ja@patrick115.eu>
2023-12-11 09:54:00 +0100

    Fixed nulls in comparer

--- CSharp/Test2/Test2/CandidateComparer.cs
+++ CSharp/Test2/Test2/CandidateComparer.cs
@@ -8,8 +8,15 @@ namespace Test2
 {
     internal class CandidateComparer : IComparer<Candidate>
     {
-        public int Compare(Candidate a, Candidate b)
+        public int Compare(Candidate? a, Candidate? b)
         {
+            if (a == null && b == null) return 0;
+            if (a == null) {
+                return -1;
+            }
+            if (b == null) {
+                return 1;
+            }
             return b.VoteCount - a.VoteCount;
         }
     }
