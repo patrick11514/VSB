444c3995b1cbb462bf489dc9231e711ebf5d2ad1
Patrik Mintěl <ja@patrick115.eu>
2023-12-12 17:47:54 +0100

    Regenerate network

--- UASS/Projekt/src/index.ts
+++ UASS/Projekt/src/index.ts
@@ -2,13 +2,14 @@ import fs from 'node:fs'
 
 const file = fs
     .readFileSync('soc-sign-bitcoinalpha.csv', 'utf-8')
+    .trim()
     .split('\n')
     .map((l) => l.trim())
 
 fs.writeFileSync('network2/network.csv', `Source;Target;From;To\n`)
 
 file.forEach((line) => {
-    const [a, b, time] = line.split(';').map((s) => s.trim())
+    const [a, b, _, time] = line.split(',').map((s) => s.trim())
 
     fs.appendFileSync('network2/network.csv', `${a};${b};${time};${time}\n`)
 })
