84243304b140ea697929ebfc539728be9983c015
patrick11514 <ja@patrick115.eu>
2024-02-20 15:01:40 +0100

    Moved to separated files

---
+++ URO/C01/main2.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class myApp:
+    def __init__(self, master: Tk):
+        master.title("10/10 UI")
+
+        self.button1 = Button(master, text=f"Čudlík 1")
+        self.button1.pack(side="left")
+
+        self.button2 = Button(master, text=f"Čudlík 2")
+        self.button2.pack(side="right")
+
+        self.button3 = Button(master, text=f"Čudlík 3")
+        self.button3.pack(side="top")
+
+        self.button4 = Button(master, text=f"Čudlík 4")
+        self.button4.pack(side="bottom")
+
+root = Tk()
+app = myApp(root)
+root.mainloop()
---
+++ URO/C01/main4.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class myApp:
+    def __init__(self, master: Tk):
+        master.title("10/10 UI")
+        
+        self.button1 = Button(master, text="Konec :/", command=master.destroy)
+        self.button1.pack(side="top", fill="both", expand=True)
+
+        self.button2 = Button(master, text="Tlačítko", command=self.write)
+        self.button2.pack(side="bottom")
+    
+    def write(self):
+        print("Ahoj")
+
+root = Tk()
+app = myApp(root)
+root.mainloop()
