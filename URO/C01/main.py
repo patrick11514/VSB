deafc55a54c37a2f99c5bce392398de2ff2f2735
patrick11514 <ja@patrick115.eu>
2024-02-20 14:45:27 +0100

    Layout 2

--- URO/C01/main.py
+++ URO/C01/main.py
@@ -7,16 +7,16 @@ class myApp:
     master.title("10/10 UI")
 
     self.button1 = Button(master, text=f"Čudlík 1")
-    self.button1.pack(side="left")
+    self.button1.pack(side="bottom")
 
     self.button2 = Button(master, text=f"Čudlík 2")
-    self.button2.pack(side="right")
+    self.button2.pack(side="bottom")
 
     self.button3 = Button(master, text=f"Čudlík 3")
-    self.button3.pack(side="top")
+    self.button3.pack(side="left")
 
     self.button4 = Button(master, text=f"Čudlík 4")
-    self.button4.pack(side="bottom")
+    self.button4.pack(side="right")
 
 
 
