b0bb2aa8e1c64a41a29fe2ddc16f1fdc2f0e6018
patrick11514 <ja@patrick115.eu>
2024-03-04 10:50:31 +0100

    Fixed symlink

--- TAMZ/C02/src/components/Ion/index.ts
+++
@@ -1,8 +0,0 @@
-import IonButton from "./ionButton.svelte";
-import IonDateTime from "./ionDateTime.svelte";
-import IonInput from "./ionInput.svelte";
-
-export {
-    IonButton, IonDateTime, IonInput
-};
-
--- TAMZ/C02/src/components/Ion/ionButton.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import type { Color } from '@ionic/core';
-
-    export let color: Color = 'primary';
-</script>
-
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<ion-button on:click {color}><slot /></ion-button>
