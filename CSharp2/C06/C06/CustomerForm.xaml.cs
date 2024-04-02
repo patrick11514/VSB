37bfdb62fd360908b2ba6b2f6216175d17a0e336
patrick11514 <ja@patrick115.eu>
2024-04-02 12:28:08 +0200

    C06

---
+++ CSharp2/C06/C06/CustomerForm.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace C06
+{
+    /// <summary>
+    /// Interakční logika pro CustomerForm.xaml
+    /// </summary>
+    public partial class CustomerForm : Window
+    {
+        public Customer Customer { get; set; }
+        public CustomerForm(Customer c = null)
+        {
+            Customer = c ?? new Customer();
+            InitializeComponent();
+
+            this.DataContext = Customer;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
