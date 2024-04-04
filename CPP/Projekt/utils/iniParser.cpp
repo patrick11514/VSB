a56c368c0be96a4ddf34819c1b6eb5e8dbd46526
patrick11514 <ja@patrick115.eu>
2024-04-04 14:18:57 +0200

    Ini file parser

--- CPP/Projekt/utils/iniParser.cpp
+++ CPP/Projekt/utils/iniParser.cpp
@@ -2,6 +2,81 @@
 
 #include <iostream>
 
+void IniParser::skipSpaces(swit &begin, const swit &end) const
+{
+    while (begin != end)
+    {
+        if (std::isspace(*begin))
+        {
+            ++begin;
+            continue;
+        }
+        break;
+    }
+}
+
+bool IniParser::isComment(swit &begin, const swit &end) const
+{
+    while (begin != end)
+    {
+        if (std::isspace(*begin))
+        {
+            ++begin;
+            continue;
+        }
+
+        if (*begin == ';')
+        {
+            return true;
+        }
+        break;
+    }
+    return false;
+}
+
+std::optional<std::string> IniParser::parseSection(swit &begin, const swit &end, size_t &error) const
+{
+    if (*begin != '[')
+    {
+        return std::nullopt;
+    }
+
+    std::string_view sw(begin, end);
+
+    auto endBracket = sw.find(']');
+    if (endBracket == sw.npos)
+    {
+        error = 1;
+        return std::nullopt;
+    }
+
+    std::string_view after(begin + endBracket + 1, end);
+    auto afterBegin = after.begin();
+    auto afterEnd = after.end();
+
+    this->skipSpaces(afterBegin, afterEnd);
+    // characters after ]
+    if (afterBegin != afterEnd)
+    {
+        error = std::distance(begin, afterBegin) + 1;
+        return std::nullopt;
+    }
+
+    return std::string(begin + 1, begin + endBracket);
+}
+
+std::optional<std::pair<std::string_view, std::string_view>> IniParser::parseKeyValue(swit &begin, const swit &end, size_t &error) const
+{
+    std::string_view sw(begin, end);
+    auto separator = sw.find('=');
+    if (separator == sw.npos)
+    {
+        error = 1;
+        return std::nullopt;
+    }
+    return std::make_pair(std::string_view(begin, begin + separator), std::string_view(begin + separator + 1, end));
+}
+
 void IniParser::logError(const std::string &filePath, const std::string_view &sw, size_t line, size_t position, const std::string &message)
 {
     this->l.error("Unable to parse file: " + filePath);
@@ -15,6 +90,37 @@ void IniParser::logError(const std::string &filePath, const std::string_view &sw
     }
     this->l.error(str + "↑");
     this->l.error(message);
+}
+
+std::string IniParser::trim(const std::string_view &str)
+{
+    auto begin = str.begin();
+    auto end = str.end();
+
+    // from start
+    while (begin != end)
+    {
+        if (std::isspace(*begin))
+        {
+            ++begin;
+            continue;
+        }
+        break;
+    }
+
+    --end;
+
+    while (begin != end)
+    {
+        if (std::isspace(*end))
+        {
+            --end;
+            continue;
+        }
+        break;
+    }
+
+    return std::string(begin, end + 1);
 }
 
 IniParser::IniParser(const std::string &filePath)
@@ -53,88 +159,143 @@ IniParser::IniParser(const std::string &filePath)
         }
 
         std::string_view sw(buffer, size);
+        auto begin = sw.begin();
+        auto end = sw.end();
+
+        this->skipSpaces(begin, end);
 
         // empty line
-        bool empty = true;
-        for (auto it = sw.begin(); it != sw.end(); ++it)
-        {
-            if (!std::isspace(*it))
-            {
-                empty = false;
-                break;
-            }
-        }
-
-        if (empty)
+        if (begin == end)
         {
             continue;
         }
 
-        // comment
-        bool comment = false;
-        for (auto it = sw.begin(); it != sw.end(); ++it)
+        if (this->isComment(begin, end))
         {
-            if (std::isspace(*it))
+            continue;
+        }
+
+        size_t error = 0;
+        auto result = this->parseSection(begin, end, error);
+
+        if (result.has_value())
+        {
+            section = result.value();
+            continue;
+        }
+
+        if (error == 1)
+        {
+            this->logError(filePath, sw, line, sw.size() + 1, "Expected ']' at the end of section");
+            exit(-1);
+        }
+
+        if (error > 1)
+        {
+            this->logError(filePath, sw, line, error, "Unexpected symbol after ']'");
+            exit(-1);
+        }
+
+        auto result2 = this->parseKeyValue(begin, end, error);
+
+        if (result2.has_value())
+        {
+            auto pair = result2.value();
+            std::string key = trim(pair.first);
+            std::string value = trim(pair.second);
+
+            std::string fullName(section);
+            fullName.reserve(section.size() + key.size());
+
+            auto it = key.end();
+
+            // check if array
+            if (*(it - 2) == '[' && *(it - 1) == ']')
             {
+                // remove [] from name
+                key.erase(key.end() - 2, key.end());
+
+                auto elem = this->keyArrayPairs.find(key);
+                if (elem == this->keyArrayPairs.end())
+                {
+                    std::vector<std::string> data;
+                    data.emplace_back(value);
+                    this->keyArrayPairs.emplace(key, data);
+                    continue;
+                }
+
+                elem->second.emplace_back(value);
                 continue;
             }
 
-            if (*it == ';')
+            auto elem = this->keyValuePairs.find(key);
+
+            if (elem == this->keyValuePairs.end())
             {
-                comment = true;
-                break;
+                this->keyValuePairs.emplace(key, value);
+                continue;
             }
 
-            break;
-        }
-
-        std::cout << sw << "||"
-                  << "c: " << comment << std::endl;
-
-        if (comment)
-        {
+            elem->second = value;
             continue;
         }
 
-        if (sw[0] == '[')
-        {
-            // section?
-            auto end = sw.find(']');
-            if (end == sw.npos)
-            {
-                this->logError(filePath, sw, line, sw.size() + 1, "Expected ']' at the end of section");
-                exit(-1);
-            }
-
-            section = std::string(sw.begin() + 1, sw.begin() + end);
-            continue;
-        }
-
-        auto separator = sw.find('=');
-
-        if (separator == sw.npos)
+        if (error > 0)
         {
             this->logError(filePath, sw, line, sw.size() + 1, "Expected '=' at the end of variable name");
             exit(-1);
         }
 
-        size_t begin = 0;
-        for (auto it = sw.begin(); it != sw.end(); ++it)
-        {
-            if (std::isspace(*it))
-            {
-                ++begin;
-                continue;
-            }
-            break;
-        }
-
-        std::string_view name(sw.begin() + begin, sw.begin() + separator);
-        std::string_view value(sw.begin() + separator + 1, sw.end());
+        this->l.error("Unable to parse file: " + filePath);
+        this->l.error("Cannot parse line " + line);
+        exit(-1);
     }
+
+    this->opened = true;
 }
 
 bool IniParser::isOpened() const
 {
     return this->opened;
+}
+
+bool IniParser::includes(const std::string &key) const
+{
+    auto it = this->keyValuePairs.find(key);
+    if (it != this->keyValuePairs.end())
+    {
+        return true;
+    }
+
+    auto it2 = this->keyArrayPairs.find(key);
+    if (it2 != this->keyArrayPairs.end())
+    {
+        return true;
+    }
+
+    return false;
+}
+
+std::optional<std::string> IniParser::getValue(const std::string &key) const
+{
+    auto it = this->keyValuePairs.find(key);
+
+    if (it == this->keyValuePairs.end())
+    {
+        return std::nullopt;
+    }
+
+    return it->second;
+}
+
+std::optional<std::vector<std::string>> IniParser::getArray(const std::string &key) const
+{
+    auto it = this->keyArrayPairs.find(key);
+
+    if (it == this->keyArrayPairs.end())
+    {
+        return std::nullopt;
+    }
+
+    return it->second;
 }
--- CPP/Projekt/utils/iniParser.hpp
+++ CPP/Projekt/utils/iniParser.hpp
@@ -11,13 +11,20 @@
 
 namespace fs = std::filesystem;
 
+using swit = std::string_view::iterator;
+
 class IniParser
 {
     bool opened = false;
     std::unordered_map<std::string, std::string> keyValuePairs;
-    std::unordered_map<std::string_view, std::vector<std::string>> keyArrayPairs;
+    std::unordered_map<std::string, std::vector<std::string>> keyArrayPairs;
 
     Logger l;
+
+    void skipSpaces(swit &begin, const swit &end) const;
+    bool isComment(swit &begin, const swit &end) const;
+    std::optional<std::string> parseSection(swit &begin, const swit &end, size_t &error) const;
+    std::optional<std::pair<std::string_view, std::string_view>> parseKeyValue(swit &begin, const swit &end, size_t &error) const;
 
     void logError(const std::string &filePath, const std::string_view &sw, size_t line, size_t position, const std::string &message);
 
@@ -25,4 +32,9 @@ public:
     IniParser(const std::string &filePath);
 
     bool isOpened() const;
+    bool includes(const std::string &key) const;
+    std::optional<std::string> getValue(const std::string &key) const;
+    std::optional<std::vector<std::string>> getArray(const std::string &key) const;
+
+    static std::string trim(const std::string_view &str);
 };
