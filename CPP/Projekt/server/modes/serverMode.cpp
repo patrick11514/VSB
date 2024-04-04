470fe4f355c323f110b3c33b69863e29b906191d
patrick11514 <ja@patrick115.eu>
2024-04-04 21:19:25 +0200

    WIP Server mode

--- CPP/Projekt/server/modes/serverMode.cpp
+++ CPP/Projekt/server/modes/serverMode.cpp
@@ -1,5 +1,9 @@
 #include "serverMode.hpp"
 
-ServerMode::ServerMode(const ArgParser &parser, Logger &logger) : MainMode(parser, logger) {}
+ServerMode::ServerMode(const ArgParser &parser, Logger &logger) : MainMode(parser, logger)
+{
+    std::string programFolder = "/etc/tondik";
+    fs::create_directory("/etc/tonda");
+}
 
 ServerMode::~ServerMode() {}
