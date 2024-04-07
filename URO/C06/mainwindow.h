73bcb2b9a78cdbea0c92029d25fdd37b1f65a3ae
Patrik Mintěl <ja@patrick115.eu>
2024-04-07 19:51:53 +0200

    URO done

--- URO/C06/mainwindow.h
+++ URO/C06/mainwindow.h
@@ -3,21 +3,57 @@
 
 #include <QWidget>
 #include <QGridLayout>
+#include <QHBoxLayout>
+#include <QFrame>
+#include <QFormLayout>
+#include <QDialog>
+
 #include <QJsonDocument>
 #include <QByteArray>
 #include <QFile>
 #include <QJsonArray>
 #include <QJsonObject>
 #include <QTableView>
-#include <QStandardItemModel>
 
+#include <QSignalMapper>
+
+#include <QStandardItemModel>
+#include <QTabWidget>
+#include <QPixmap>
+
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+
+struct Item;
 class MainWindow : public QWidget
 {
     Q_OBJECT
 
+    QFormLayout *data;
+    QPixmap *map;
+    QLabel *preview;
+    std::array<QLabel *, 4> images;
+    std::vector<Item> mainData;
+    int currentIndex = -1;
+
+    QDialog *opened = nullptr;
+
+    void openNewWindow(bool edit);
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void onSelect(QModelIndex index);
+
+    void newItem();
+    void editItem();
+    void removeItem();
+
+    void closeWindow();
+    void confirmWindow();
 };
 
 struct Item
