6dc4c24dbaa156c9f589479ea0eb433c50d45cc7
patrick11514 <ja@patrick115.eu>
2024-04-08 21:03:23 +0200

    DROP database

---
+++ DS2/Drop.sql
@@ -0,0 +1,41 @@
+/*
+Created: 16/10/2023
+Model: The Chamber of Deputies, The Czech Republic
+Company: FEI VSB-TU Ostrava
+Script author for SQL Server: Radim Baca
+Version for Oracle: Michal Fait, February 2024
+Source: https://www.psp.cz/sqw/hp.sqw?k=1301, https://www.psp.cz/sqw/hp.sqw?k=1302
+*/
+
+drop table bod_schuze;
+drop table bod_schuze_ext;
+drop table zmatecne;
+drop table zmatecne_ext;
+drop table zmatecne_hlasovani_poslanec;
+drop table zmatecne_hlasovani_poslanec_ext;
+drop table funkce;
+drop table funkce_ext;
+drop table typ_funkce;
+drop table typ_funkce_ext;
+drop table omluva;
+drop table omluva_ext;
+drop table hlasovani_poslanec;
+drop table hlasovani_poslanec_ext;
+drop table hlasovani;
+drop table hlasovani_ext;
+drop table schuze_stav;
+drop table schuze_stav_ext;
+drop table schuze;
+drop table schuze_ext;
+drop table poslanec;
+drop table poslanec_ext;
+drop table organ;
+drop table organ_ext;
+drop table typ_organu;
+drop table typ_organu_ext;
+drop table zarazeni;
+drop table zarazeni_ext;
+drop table osoba;
+drop table osoba_ext;
+drop table log;
+
