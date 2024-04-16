280c9106d660efeec34f709a8f0fdc93fecbd92b
patrick11514 <ja@patrick115.eu>
2024-04-16 18:47:10 +0200

    C07

---
+++ TAMZ/C07/vite/config.dev.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+	],
+	server: {
+		port: 8080
+	}
+})
