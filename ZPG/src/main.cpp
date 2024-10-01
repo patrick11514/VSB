a245bb8aa49ddb25effaa4ffa0f307cf89639d95
patrick11514 <ja@patrick115.eu>
2024-10-01 13:17:08 +0200

    Change color of rectangle

--- ZPG/src/main.cpp
+++ ZPG/src/main.cpp
@@ -50,15 +50,18 @@ float points[] = {
 const char *vertex_shader =
     "#version 330\n"
     "layout(location=0) in vec3 vp;"
+    "out vec3 color;"
     "void main () {"
     "     gl_Position = vec4 (vp, 1.0);"
+    "     color = vp;"
     "}";
 
 const char *fragment_shader =
     "#version 330\n"
+    "in vec3 color;"
     "out vec4 frag_colour;"
     "void main () {"
-    "     frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+    "     frag_colour = vec4 (color, 1.0);"
     "}";
 
 int main(void)
