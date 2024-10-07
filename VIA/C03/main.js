edaa18bbb1fc492da6c3f8692c1514b9ea061445
patrick11514 <ja@patrick115.eu>
2024-10-07 10:51:48 +0200

    Copy C01 to C02 + fill whole page with table

---
+++ VIA/C03/main.js
@@ -0,0 +1,358 @@
+const days = {
+    PO: 'Pondělí',
+    UT: 'Úterý',
+    ST: 'Středa',
+    CT: 'Čtvrtek',
+    PA: 'Pátek'
+};
+
+/**
+ * @type {{
+ *  day: keyof days,
+ *  name: string,
+ *  room: string,
+ *  group: string,
+ *  additionalInfo?: string,
+ *  from: Date,
+ *  to: Date,
+ *  type: "Lecture" | "Excercise",
+ *  teacher: string
+ * }[]}
+ */
+const DATA = [
+    {
+        day: 'PO',
+        name: 'ZPG',
+        room: 'POREC1',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 1, 9, 0),
+        to: new Date(2024, 1, 1, 10, 30),
+        type: 'Lecture',
+        teacher: 'M. Němec'
+    },
+    {
+        day: 'PO',
+        name: 'VIA',
+        room: 'POREB213',
+        group: 'C/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 1, 10, 45),
+        to: new Date(2024, 1, 1, 12, 15),
+        type: 'Excercise',
+        teacher: 'M. Radecký'
+    },
+    {
+        day: 'PO',
+        name: 'VIA',
+        room: 'POREC2',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 1, 12, 30),
+        to: new Date(2024, 1, 1, 14, 0),
+        type: 'Lecture',
+        teacher: 'M. Radecký'
+    },
+    {
+        day: 'UT',
+        name: 'OSY',
+        room: 'POREC2',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 2, 7, 15),
+        to: new Date(2024, 1, 2, 8, 45),
+        type: 'Lecture',
+        teacher: 'P. Olivka'
+    },
+    {
+        day: 'UT',
+        name: 'SPR',
+        room: 'PORJA401',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 2, 9, 0),
+        to: new Date(2024, 1, 2, 10, 30),
+        type: 'Lecture',
+        teacher: 'Z. Sawa'
+    },
+    {
+        day: 'UT',
+        name: 'SPR',
+        room: 'PORJA401',
+        group: 'C/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 2, 10, 45),
+        to: new Date(2024, 1, 2, 12, 15),
+        type: 'Excercise',
+        teacher: 'Z. Sawa'
+    },
+    {
+        day: 'UT',
+        name: 'ZPG',
+        room: 'POREB408',
+        group: 'C/07',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 2, 12, 30),
+        to: new Date(2024, 1, 2, 14, 0),
+        type: 'Excercise',
+        teacher: 'M. Němec'
+    },
+    {
+        day: 'UT',
+        name: 'VIS',
+        room: 'POREC3',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 14, 15),
+        to: new Date(2024, 1, 3, 15, 45),
+        type: 'Lecture',
+        teacher: 'M. Kudělka'
+    },
+    {
+        day: 'ST',
+        name: 'ZSU',
+        room: 'POREC1',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 9, 0),
+        to: new Date(2024, 1, 3, 10, 30),
+        type: 'Lecture',
+        teacher: 'J. Platoš'
+    },
+    {
+        day: 'ST',
+        name: 'TAMZ II',
+        room: 'POREC3',
+        group: 'P/01',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 10, 45),
+        to: new Date(2024, 1, 3, 12, 15),
+        type: 'Lecture',
+        teacher: 'M. Krumnikl'
+    },
+    {
+        day: 'ST',
+        name: 'VIS',
+        room: 'POREB406',
+        group: 'C/05',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 12, 30),
+        to: new Date(2024, 1, 3, 14, 0),
+        type: 'Excercise',
+        teacher: 'T. Anlauf'
+    },
+    {
+        day: 'CT',
+        name: 'TAMZ II',
+        room: 'POREB406',
+        group: 'C/02',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 9, 0),
+        to: new Date(2024, 1, 3, 10, 30),
+        type: 'Excercise',
+        teacher: 'R. Fusek'
+    },
+    {
+        day: 'CT',
+        name: 'OSY',
+        room: 'POREB430',
+        group: 'C/10',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 11, 30),
+        to: new Date(2024, 1, 3, 14, 0),
+        type: 'Excercise',
+        teacher: 'P. Olivka'
+    },
+    {
+        day: 'CT',
+        name: 'ZSU',
+        room: 'PORRV203',
+        group: 'C/02',
+        additionalInfo: 'Nepravidelně',
+        from: new Date(2024, 1, 3, 14, 15),
+        to: new Date(2024, 1, 3, 15, 45),
+        type: 'Excercise',
+        teacher: 'M. Hanuš'
+    }
+];
+
+/**
+ * @type {Record<keyof days, {
+ *  name: string,
+ *  room: string,
+ *  group: string,
+ *  additionalInfo?: string,
+ *  from: Date,
+ *  to: Date,
+ *  type: "Lecture" | "Excercise"
+ * }[]>}
+ */
+const agregated = {};
+
+for (const day of Object.keys(days)) {
+    const data = DATA.filter((item) => item.day === day);
+    data.sort((a, b) => a.from - b.from);
+    agregated[day] = data;
+}
+
+const tbody = document.querySelector('tbody');
+const dayStart = new Date(2024, 1, 1, 7, 15);
+const dayEnd = new Date(2024, 1, 1, 19, 15);
+let currentDay = new Date(dayStart);
+
+/**
+ * Mapp times for example 8:45 -> 9:00 as start of next subject, or 9:45 -> 9:45
+ * @type {Record<string, string>}
+ */
+const getStartFromEnd = {};
+
+/**
+ * Convert date to string in format HH:MM
+ * @param {Date} date
+ * @returns {string}
+ */
+const getTime = (date) => {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}`;
+};
+
+/**
+ * Convert time string to Date object
+ * @param {string} time
+ * @returns {Date}
+ */
+const getDate = (time) => {
+    const [hours, minutes] = time.split(':');
+    const date = new Date(currentDay);
+    date.setHours(parseInt(hours));
+    date.setMinutes(parseInt(minutes));
+
+    return date;
+};
+
+let spaces = 0;
+let add = false;
+
+currentDay.setMinutes(currentDay.getMinutes() + 45);
+
+while (true) {
+    const from = getTime(currentDay);
+
+    if (spaces == 1) {
+        currentDay.setMinutes(currentDay.getMinutes() + 15);
+    }
+
+    const to = getTime(currentDay);
+
+    currentDay.setMinutes(currentDay.getMinutes() + 45);
+    spaces++;
+
+    if (spaces == 2) {
+        spaces = 0;
+    }
+
+    getStartFromEnd[from] = to;
+
+    if (currentDay.getTime() >= dayEnd.getTime()) {
+        break;
+    }
+}
+
+currentDay = new Date(dayStart);
+
+for (const [day, subjects] of Object.entries(agregated)) {
+    currentDay = new Date(dayStart);
+    const tr = document.createElement('tr');
+    const dayName = document.createElement('td');
+    dayName.innerText = days[day];
+    tr.appendChild(dayName);
+
+    /**
+     * @type {typeof DATA[number] | undefined}
+     */
+    let popped = undefined;
+    let spaces = 0;
+
+    while (true) {
+        if (!popped) {
+            popped = subjects.shift();
+        }
+
+        if (popped && popped.from.getHours() === currentDay.getHours() && popped.from.getMinutes() === currentDay.getMinutes()) {
+            const td = document.createElement('td');
+            td.classList.add(popped.type === 'Excercise' ? 'excercises' : 'lecture');
+            const blocks = Math.floor((popped.to.getTime() - popped.from.getTime()) / 1000 / 60 / 45);
+            td.colSpan = blocks;
+
+            const subjectInfo = document.createElement('div');
+            subjectInfo.classList.add('row');
+
+            const link = document.createElement('a');
+            link.href = '#';
+            link.innerText = popped.name;
+            subjectInfo.appendChild(link);
+
+            if (popped.additionalInfo) {
+                const note = document.createElement('span');
+                note.innerText = popped.additionalInfo;
+                note.classList.add('note');
+                subjectInfo.appendChild(note);
+            }
+
+            const icon = document.createElement('div');
+            icon.classList.add('icon');
+            subjectInfo.appendChild(icon);
+            td.appendChild(subjectInfo);
+
+            const teacher = document.createElement('div');
+            teacher.innerText = popped.teacher;
+            td.appendChild(teacher);
+
+            const room = document.createElement('div');
+            room.classList.add('row');
+
+            const roomName = document.createElement('div');
+            roomName.innerText = popped.room;
+            room.appendChild(roomName);
+
+            const group = document.createElement('div');
+            group.innerText = popped.group;
+            group.classList.add('end');
+            room.appendChild(group);
+
+            td.appendChild(room);
+
+            tr.appendChild(td);
+
+            //set current time to end of subject
+            currentDay = getDate(getStartFromEnd[getTime(popped.to)]);
+
+            popped = undefined;
+
+            //skip loop to check if there is another subject
+            continue;
+        } else {
+            //fill empty cell
+            const td = document.createElement('td');
+            tr.appendChild(td);
+        }
+
+        ++spaces;
+
+        currentDay.setMinutes(currentDay.getMinutes() + 45);
+
+        if (spaces == 2) {
+            //add another 15 minutes, if we want to skip break;
+            currentDay.setMinutes(currentDay.getMinutes() + 15);
+            spaces = 0;
+        }
+
+        if (currentDay.getTime() >= dayEnd.getTime()) {
+            break;
+        }
+    }
+
+    tbody.appendChild(tr);
+}
