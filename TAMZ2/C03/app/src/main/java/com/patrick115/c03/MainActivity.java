43b7e31757cb2c762a16364b3cee51cbfa1a73c7
Patrik Mintěl <ja@patrick115.eu>
2024-10-16 23:39:15 +0200

    Finished homework

--- TAMZ2/C03/app/src/main/java/com/patrick115/c03/MainActivity.java
+++ TAMZ2/C03/app/src/main/java/com/patrick115/c03/MainActivity.java
@@ -2,6 +2,9 @@ package com.patrick115.c03;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,23 +41,116 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        LineChart chart = findViewById(R.id.chart);
-        chart.setBackgroundColor(Color.LTGRAY);
+        BarChart chart = findViewById(R.id.chart);
+        chart.setBackgroundColor(Color.WHITE);
+        chart.setPinchZoom(false);
+        chart.setDrawGridBackground(false);
 
-        List<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(0,0));
-        entries.add(new Entry(5,5));
-        entries.add(new Entry(10,5));
-        entries.add(new Entry(15,15));
-        entries.add(new Entry(20,20));
+        Description desc = new Description();
+        desc.setText("");
+        chart.setDescription(desc);
 
 
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        TextView money = findViewById(R.id.moneyPut);
+        TextView interest = findViewById(R.id.interest);
+        TextView period = findViewById(R.id.period);
 
-        dataSet.setLineWidth(20);
+        SeekBar seekMoney = findViewById(R.id.moneySet);
+        seekMoney.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                money.setText(getString(R.string.money_put, progress * 10000));
+                recalculate();
+            }
 
-        LineData lineData   = new LineData(dataSet);
-        chart.setData(lineData);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar seekInterest = findViewById(R.id.interestBar);
+        seekInterest.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                interest.setText(getString(R.string.interest_put, progress));
+                recalculate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar seekPeriod = findViewById(R.id.periodBar);
+        seekPeriod.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                period.setText(getString(R.string.period_put, progress));
+                recalculate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        recalculate();
+    }
+
+    private void recalculate() {
+        SeekBar seekMoney = findViewById(R.id.moneySet);
+        SeekBar seekInterest = findViewById(R.id.interestBar);
+        SeekBar seekPeriod = findViewById(R.id.periodBar);
+
+        TextView savedMoney = findViewById(R.id.savedMoney);
+        TextView savedInterest = findViewById(R.id.savedDiff);
+
+        int money = seekMoney.getProgress() * 10000;
+
+        double result = money * Math.pow(1 + (float)seekInterest.getProgress() / 100, seekPeriod.getProgress());
+        double interest = result - money;
+        savedMoney.setText(getString(R.string.money, (int)result));
+        savedInterest.setText(getString(R.string.interest, (int)interest));
+
+        BarChart chart = findViewById(R.id.chart);
+
+        float graphMin = Math.max(0, (float)Math.min(money, interest) - 10000);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setCenterAxisLabels(true);
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setAxisMinimum(graphMin);
+        YAxis yAxis2 = chart.getAxisRight();
+        yAxis2.setAxisMinimum(graphMin);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, money));
+
+        ArrayList<BarEntry> entries2 = new ArrayList<>();
+        entries2.add(new BarEntry(1, (float) interest));
+
+        BarDataSet data = new BarDataSet(entries, "Vklad");
+        BarDataSet data2 = new BarDataSet(entries2, "Úroky");
+        data.setColor(Color.CYAN);
+        chart.setData(new BarData(data, data2));
         chart.invalidate();
     }
 }
