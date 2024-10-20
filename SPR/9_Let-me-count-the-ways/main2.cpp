5a8582a677397d3a74dc8d916edd02c160f43d8d
patrick11514 <ja@patrick115.eu>
2024-10-21 00:32:37 +0200

    YIPEEEE

--- SPR/9_Let-me-count-the-ways/main2.cpp
+++ SPR/9_Let-me-count-the-ways/main2.cpp
@@ -5,7 +5,7 @@
 static int coins[]{50, 25, 10, 5, 1};
 
 // For each layer 0-4 we have map, which caches the result
-static std::array<std::unordered_map<int, int>, 5> cached;
+static std::array<std::unordered_map<int, long>, 5> cached;
 
 void print(std::array<int, 5> &solution) {
   for (auto item : solution) {
@@ -22,41 +22,36 @@ int sumCents(std::array<int, 5> &solution, int index = 0) {
   return sum;
 }
 
-int solve(int cents, std::array<int, 5> solution, int index, int count = 0) {
-  printf("Cents: %d, Index %d, Count: %d\n", cents, index, count);
+long solve(int cents, std::array<int, 5> solution, int index, long count = 0) {
   if (index == 4) {
-    solution[index] = cents - sumCents(solution, index);
-    print(solution);
     return count + 1;
   }
 
   auto res = cached[index].find(cents);
   if (res != cached[index].end()) {
-    printf("Loaded: %d \n", res->second);
-    print(solution);
     return res->second + count;
   }
 
-  int rawCount = 0;
+  long rawCount = 0;
 
   while (true) {
     if (sumCents(solution, index) > cents) {
       break;
     }
 
-    rawCount = count;
-
+    long pre = count;
     count =
         solve(cents - sumCents(solution, index), solution, index + 1, count);
 
+    // only add diffs, so we get raw count for this subset
+    rawCount += count - pre;
+
     solution[index]++;
   }
-  if (cents > 0) {
-    printf("Saving %d coins index %d count %d\n", cents, index, count);
-    cached[index].emplace(cents, count - rawCount);
-  }
+  // if (cents > 0) {
+  cached[index].emplace(cents, rawCount);
+  //}
 
-  printf("%d coins have %d ways, index: %d\n", cents, count, index);
   return count;
 }
 
@@ -67,10 +62,10 @@ int main() {
     if (read < 1)
       break;
 
-    int ways = solve(cents, {0, 0, 0, 0, 0}, 0);
+    long ways = solve(cents, {0, 0, 0, 0, 0}, 0);
 
     if (ways > 1) {
-      printf("There are %d ways to produce %d cents change.\n", ways, cents);
+      printf("There are %ld ways to produce %d cents change.\n", ways, cents);
 
     } else {
       printf("There is only 1 way to produce %d cents change.\n", cents);
