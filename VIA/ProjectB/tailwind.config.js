c0fd7f2b74d98726afd0a444b3e9c421f750cd7f
Patrik Mintěl <ja@patrick115.eu>
2024-11-03 23:57:05 +0100

    Main screen done

--- VIA/ProjectB/tailwind.config.js
+++ VIA/ProjectB/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js,css}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                text: "#171A20"
+            },
+            fontFamily: {
+                "universal-sans": "UniversalSans, Arial, sans-serif"
+            },
+            height: {
+                hero: "calc(100vh - 44px)"
+            }
+        },
     },
     plugins: [],
 }
