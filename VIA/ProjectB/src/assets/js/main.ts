2d5e053f27316a3f4153d12e39516e9203ffaae0
Patrik Mintěl <ja@patrick115.eu>
2024-11-19 22:08:08 +0100

    Moved SVGS, downloaded images, WIP cybertruck

--- VIA/ProjectB/src/assets/js/main.ts
+++ VIA/ProjectB/src/assets/js/main.ts
@@ -1,1 +1,21 @@
-console.log('Ahoj');
+const load = async (svg: SVGElement) => {
+    const attr = svg.getAttribute('load');
+    if (!attr) return;
+
+    const response = await fetch(`/assets/svgs/${attr}`);
+    const html = await response.text();
+    const temp = document.createElement('div');
+    temp.innerHTML = html;
+    const child = temp.firstChild as SVGElement | null;
+
+    if (!child) return;
+
+    svg.classList.forEach((c) => child.classList.add(c));
+
+    svg.replaceWith(child);
+};
+
+const svgs = Array.from(document.querySelectorAll('svg'))
+    .filter((el) => el.hasAttribute('load'))
+    .map(load);
+Promise.all(svgs);
