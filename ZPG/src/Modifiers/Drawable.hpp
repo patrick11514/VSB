014890e21ee21d10f6fbe07720ae12cdc666b538
patrick11514 <ja@patrick115.eu>
2024-11-20 17:15:22 +0100

    WIP ZPG

--- ZPG/src/Modifiers/Drawable.hpp
+++ ZPG/src/Modifiers/Drawable.hpp
@@ -5,11 +5,14 @@
 
 #include "../Object/Material/Material.hpp"
 #include "../Object/ObjectData.hpp"
+#include "../Object/Texture/Texture.hpp"
 #include "../Shader/ShaderProgram.hpp"
 #include "BasicAttribute.hpp"
 
 // Define custom types
 using RenderFunction = std::function<void()>;
+
+enum TextureType { MATERIAL, TEXTURE };
 
 class Drawable : public BasicAttribute {
 protected:
@@ -18,13 +21,25 @@ protected:
       [[maybe_unused]]; ///< Shader program used on object
   RenderFunction
       renderFunction; ///< Function which will be called to render the object
+
+  TextureType textureType;
+
   std::shared_ptr<Material> material;
+  std::shared_ptr<Texture> texture;
 
 public:
   Drawable(std::shared_ptr<ObjectData> data, ShaderProgram *shaderProgram,
            RenderFunction renderFunction, std::shared_ptr<Material> material)
       : data(data), shaderProgram(shaderProgram),
-        renderFunction(renderFunction), material(material) {};
+        renderFunction(renderFunction), textureType(TextureType::MATERIAL),
+        material(material) {};
+
+  Drawable(std::shared_ptr<ObjectData> data, ShaderProgram *shaderProgram,
+           RenderFunction renderFunction, std::shared_ptr<Texture> texture)
+      : data(data), shaderProgram(shaderProgram),
+        renderFunction(renderFunction), textureType(TextureType::TEXTURE),
+        texture(texture) {};
+
   virtual ~Drawable() = default;
 
   virtual void update() const;
