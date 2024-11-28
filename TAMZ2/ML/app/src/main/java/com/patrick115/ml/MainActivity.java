27031c0dea2ba25155b58ef7d918761728f1d3be
patrick11514 <ja@patrick115.eu>
2024-11-28 12:28:18 +0100

    WIP MachineLearning

--- TAMZ2/ML/app/src/main/java/com/patrick115/ml/MainActivity.java
+++ TAMZ2/ML/app/src/main/java/com/patrick115/ml/MainActivity.java
@@ -1,12 +1,24 @@
 package com.patrick115.ml;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FaceDetectorOptions highAccuracyOpts =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                        .build();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.img2);
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+
+        FaceDetector detector = FaceDetection.getClient(highAccuracyOpts);
+
+        detector.process(image).addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+            @Override
+            public void onSuccess(List<Face> faces) {
+                float smileProb = faces.get(0).getSmilingProbability();
+                Toast.makeText(MainActivity.this, "face: " + smileProb, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
