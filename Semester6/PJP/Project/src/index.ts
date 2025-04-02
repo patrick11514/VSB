83ef3c9f2f8e6162e0dab35490c83b1ff28d48f5
patrick11514 <ja@patrick115.eu>
2025-04-03 00:11:51 +0200

    Some project things

--- Semester6/PJP/Project/src/index.ts
+++ Semester6/PJP/Project/src/index.ts
@@ -1,17 +1,24 @@
-import Logger from './lib/logger';
-//logger for main messages
-const l = new Logger('MyCoolApp', 'cyan');
+import fs from 'node:fs';
+import ArrayInitLexer from './lib/Generated/ArrayInitLexer';
+import ArrayInitParser from './lib/Generated/ArrayInitParser';
+import { CharStream, CommonTokenStream, ParseTreeWalker } from 'antlr4';
+import ValueListener from './lib/ValueListener';
+import TransformVisitor from './lib/TransformVisitor';
 
-type Cat = {
-    name: string;
-    age: number;
-    color: 'white' | 'orange' | 'black' | 'gray';
-};
+const data = fs.readFileSync('input.txt', 'utf8');
+const charStream = new CharStream(data);
+const lexer = new ArrayInitLexer(charStream);
+const tokenStream = new CommonTokenStream(lexer);
+const parser = new ArrayInitParser(tokenStream);
+const tree = parser.init();
+console.log(tree.toStringTree(parser.ruleNames, parser));
 
-const myCuteAnimal = {
-    name: 'Mourek',
-    age: 2,
-    color: 'orange'
-} satisfies Cat;
+if (parser.syntaxErrorsCount == 0) {
+    const walker = new ParseTreeWalker();
+    walker.walk(new ValueListener(), tree);
 
-l.log(JSON.stringify(myCuteAnimal));
+    const transform = new TransformVisitor();
+    console.log(transform.visit(tree));
+} else {
+    console.log('ERRORS: ', parser.syntaxErrorsCount);
+}
