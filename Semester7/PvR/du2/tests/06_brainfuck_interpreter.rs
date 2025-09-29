a93a5acddead025e9e8b5b79e88c7f8e28555b19
patrick11514 <ja@patrick115.eu>
2025-09-29 20:09:42 +0200

    WIP

--- Semester7/PvR/du2/tests/06_brainfuck_interpreter.rs
+++ Semester7/PvR/du2/tests/06_brainfuck_interpreter.rs
@@ -13,10 +13,123 @@
 // Hint: Put `#[derive(Debug, Eq, PartialEq)]` on top of `ParseError`, `ExecuteError` and `Program`
 // (and any other custom types nested inside them) so that asserts in tests work.
 
+#[derive(Debug, Eq, PartialEq)]
+enum ParseError {
+    UnknownInstruction { location: usize, instruction: char },
+    UnmatchedLoop { location: usize },
+}
+#[derive(Debug, Eq, PartialEq)]
+enum ExecuteError {
+    NoInputLeft,
+    InfiniteLoop,
+}
+#[derive(Debug, Eq, PartialEq)]
+enum Instruction {
+    IncrementDP,
+    DecrementDP,
+    IncrementB,
+    DecrementB,
+    AcceptByte,
+    OutputByte,
+    LoopStart { loop_start: usize },
+    LoopEnd { loop_end: usize },
+}
+#[derive(Debug, Eq, PartialEq)]
+struct Program {
+    instructions: Vec<Instruction>,
+}
+
+impl Program {
+    fn execute(&self, input: Vec<u8>, memory: Vec<u8>) -> Result<String, ExecuteError> {
+        let mut memory = memory;
+
+        let mut ip = 0;
+        let mut instructions_executed = 0;
+        let mut data_pointer = 0;
+        let mut input_pointer = 0;
+
+        let mut output = String::new();
+        let mut data_ptr = 0;
+
+        loop {
+            if instructions_executed > 1000 {
+                return Err(ExecuteError::InfiniteLoop);
+            }
+
+            let instruction = &self.instructions[ip];
+
+            match instruction {
+                Instruction::IncrementDP => {
+                    data_pointer += 1;
+                }
+                Instruction::DecrementDP => {
+                    data_pointer -= 1;
+                }
+                Instruction::IncrementB => {
+                    memory[data_pointer] += 1;
+                }
+                Instruction::DecrementB => {
+                    memory[data_pointer] -= 1;
+                }
+                Instruction::AcceptByte => {
+                    memory[data_pointer] = input[input_pointer];
+                    input_pointer += 1;
+                }
+                Instruction::LoopStart { loop_start } => {}
+                Instruction::LoopEnd { loop_end } => todo!(),
+            }
+
+            ip += 1;
+            instructions_executed += 1;
+        }
+
+        Ok(output)
+    }
+}
+
+fn parse_program(program: &str) -> Result<Program, ParseError> {
+    let mut stack = Vec::<usize>::new();
+
+    let chars = program.chars();
+    let mut instructions = Vec::<Instruction>::with_capacity(chars.clone().count());
+    for (i, char) in chars.enumerate() {
+        match char {
+            '>' => instructions.push(Instruction::IncrementDP),
+            '<' => instructions.push(Instruction::DecrementDP),
+            '+' => instructions.push(Instruction::IncrementB),
+            '-' => instructions.push(Instruction::DecrementB),
+            '.' => instructions.push(Instruction::OutputByte),
+            ',' => instructions.push(Instruction::AcceptByte),
+            '[' => {
+                stack.push(i);
+                instructions.push(Instruction::LoopStart { loop_start: i });
+            }
+            ']' => match stack.pop() {
+                None => return Err(ParseError::UnmatchedLoop { location: i }),
+                Some(_) => {
+                    instructions.push(Instruction::LoopEnd { loop_end: i });
+                }
+            },
+            _ => {
+                return Err(ParseError::UnknownInstruction {
+                    location: i,
+                    instruction: char,
+                });
+            }
+        }
+    }
+
+    if !stack.is_empty() {
+        return Err(ParseError::UnmatchedLoop { location: stack[0] });
+    }
+
+    Ok(Program { instructions })
+}
+
 /// Below you can find a set of unit tests.
 #[cfg(test)]
 mod tests {
-    use crate::{parse_program, ExecuteError, ParseError};
+    use crate::{ExecuteError, ParseError, parse_program};
 
     #[test]
     fn parse_empty() {
@@ -81,7 +194,11 @@ mod tests {
 
     #[test]
     fn hello_world() {
-        check_output("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", "", "Hello World!\n");
+        check_output(
+            "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.",
+            "",
+            "Hello World!\n",
+        );
     }
 
     fn check_output(program_text: &str, input: &str, expected_output: &str) {
