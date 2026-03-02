d1c737954b9880fa518c555f0d0c07a6b05b8d2a
patrick11514 <ja@patrick115.eu>
2026-03-02 09:13:45 +0100

    BADM C02

---
+++ Semester8/BADM/C02/fieldelement.py
@@ -0,0 +1,107 @@
+from unittest import TestCase
+
+class FieldElement:
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other: FieldElement):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        
+        # TODO 1 - implement addition
+        return FieldElement((self.num + other.num) % self.prime, self.prime)
+
+    def __sub__(self, other: FieldElement):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        
+        # TODO 2 - implement subtraction
+        return FieldElement((self.num - other.num) % self.prime, self.prime)
+
+    def __mul__(self, other: FieldElement):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        
+        # TODO 3 - implement multiplication
+        return FieldElement((self.num * other.num) % self.prime, self.prime)
+
+    def __pow__(self, exponent: int):
+        # TODO 4 - implement power
+        return FieldElement(pow(self.num, exponent, self.prime), self.prime)
+
+    def __truediv__(self, other: FieldElement):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+  
+        # TODO 5 - implement division
+        inv = other ** (other.prime - 2)
+        return self * inv
+
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+
+        return self.__class__(num=num, prime=self.prime)
+
+
+class FieldElementTest(TestCase):
+    def test_add(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(15, 31)
+        self.assertEqual(a + b, FieldElement(17, 31))
+
+        a = FieldElement(17, 31)
+        b = FieldElement(21, 31)
+        self.assertEqual(a + b, FieldElement(7, 31))
+
+    def test_sub(self):
+        a = FieldElement(29, 31)
+        b = FieldElement(4, 31)
+        self.assertEqual(a - b, FieldElement(25, 31))
+
+        a = FieldElement(15, 31)
+        b = FieldElement(30, 31)
+        self.assertEqual(a - b, FieldElement(16, 31))
+
+    def test_mul(self):
+        a = FieldElement(24, 31)
+        b = FieldElement(19, 31)
+        self.assertEqual(a * b, FieldElement(22, 31))
+
+    def test_pow(self):
+        a = FieldElement(17, 31)
+        self.assertEqual(a**3, FieldElement(15, 31))
+        
+        a = FieldElement(5, 31)
+        b = FieldElement(18, 31)
+        self.assertEqual(a**5 * b, FieldElement(16, 31))
+
+    def test_div(self):
+       a = FieldElement(3, 31)
+       b = FieldElement(24, 31)
+       self.assertEqual(a / b, FieldElement(4, 31))
+
+       a = FieldElement(17, 31)
+       self.assertEqual(a**-3, FieldElement(29, 31))
+
+       a = FieldElement(4, 31)
+       b = FieldElement(11, 31)
+       self.assertEqual(a**-4 * b, FieldElement(13, 31))
+
+
